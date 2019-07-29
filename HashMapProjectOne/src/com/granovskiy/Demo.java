8901b10e69a37b80b87b9165507036549607c5e4
Sviatoslav <s.granovskiy@gmail.com>
2019-07-29 13:11:34 +0300

    some improvements made

--- HashMapProjectOne/src/com/granovskiy/Demo.java
+++ HashMapProjectOne/src/com/granovskiy/Demo.java
@@ -1,19 +1,15 @@
 package com.granovskiy;
 
 import java.util.HashMap;
-
-import static com.granovskiy.RandomNumbers.generateRandomInt;
-import static com.granovskiy.RandomNumbers.generateRandomLong;
+import java.util.Random;
 
 public class Demo {
     public static void main(String[] args) {
         HashMap<Integer, Long> numbers = new HashMap<>();
         int size = 10;
         for (int i = 0; i < size; i++) {
-            numbers.put(generateRandomInt(), generateRandomLong());
+            numbers.put(new Random().nextInt(), new Random().nextLong());
         }
-
         System.out.println(numbers);
-        //System.out.println(numbers.get(2));
     }
 }
