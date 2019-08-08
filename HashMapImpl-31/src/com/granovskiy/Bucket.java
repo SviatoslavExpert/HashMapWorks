8f84efd8f3fc7001b3070d333f1f92ed171a0f71
Sviatoslav <s.granovskiy@gmail.com>
2019-08-08 15:18:31 +0300

    trying to add node to bucketList

--- HashMapImpl-31/src/com/granovskiy/Bucket.java
+++ HashMapImpl-31/src/com/granovskiy/Bucket.java
@@ -9,6 +9,13 @@ public class Bucket {
     private int bucketSize;
     private List<Node> bucketList;
 
+    //   trying to add node to the bucketList as a structural thing
+    public Bucket() {
+        node = new Node(new Car(0), new Driver(" "));
+        bucketSize = 18;
+        bucketList = new ArrayList<>(bucketSize);
+        bucketList.add(index, node);
+    }
 
     public Bucket(int bucketSize) {
         this.bucketSize = bucketSize;
